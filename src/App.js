a58809fe090e70d98f9d6f3387e63fe5291d7721
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-10-07 05:38:23 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import {getAuth,signInWithPopup,GoogleAuthProvider} from 'firebase/auth';
 import './App.css';
+import initializeAuthentication from './Firebase/Firebase.initialize';
+initializeAuthentication();
+const provider =new GoogleAuthProvider;
 
 function App() {
+  const handleGoogleSignIn=()=>{
+    //5 number step ta dekhe dekhe kortesi.
+  const auth=getAuth();
+  //signInWithProvider ke amader auth ar provider ta pass kore dite hobe. 
+ signInWithPopup(auth,provider)
+ .then(result=>{
+  const user =result.user;
+  console.log(user);
+ })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <button onClick={handleGoogleSignIn}>Google Sign in</button>
     </div>
   );
 }
---
+++ src/Firebase/Firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAAJJE5jrUt8Cu1k3hRB5XbD9SHTZVBiUw",
+  authDomain: "simple-login-authenticat-c1222.firebaseapp.com",
+  projectId: "simple-login-authenticat-c1222",
+  storageBucket: "simple-login-authenticat-c1222.appspot.com",
+  messagingSenderId: "86792348640",
+  appId: "1:86792348640:web:7b31cde971ba5a34ac4185"
+};
+export default firebaseConfig;
