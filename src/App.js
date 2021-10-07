a252c2026c98ff2afeb6755d6b3ca7f8f6053b6e
Faisal-Khan-Tushar <faisal15-13400@diu.edu.bd>
2021-10-07 07:09:53 +0600

    showing informatoins in the ui

--- src/App.js
+++ src/App.js
@@ -2,22 +2,43 @@ import './App.css';
 
 import {  getAuth, signInWithPopup,GoogleAuthProvider } from "firebase/auth";
 import initializeAuthentication from './Firebase/Firebase.initialize';
+import { useState } from 'react';
 initializeAuthentication();
 const provider = new GoogleAuthProvider();
 
 function App() {
+  const [user,setUser]=useState({})
 const handleGoogleSignIn=()=>{
   const auth=getAuth();
   signInWithPopup(auth,provider)
   .then(result=>{
     //result er moddhei ekjon user name ache tai oita ami user variable e niye ashlam.
-    const user=result.user;
-    console.log(user)
+
+    //state er user er sathe jeno kahini na kore tai ei naem ta chaneg kore looginUser dilam.
+    const {displayName,email,photoURL}=result.user;
+     const loggedInUser={
+       name:displayName,
+       email:email,
+       photo:photoURL
+     };
+     setUser(loggedInUser);
+  })
+  .catch(error=>{
+    console.log(error.message)
   })
 }
   return (
     <div className="App">
      <button onClick={handleGoogleSignIn}>Google sign in</button>
+     <br/>
+     {
+       user.email && <div>
+         <h2>Welcome {user.name}</h2>
+         <p>I know your email address:{user.email}</p>
+         <img src={user.photo} alt="" />
+         </div>
+        
+     }
     </div>
   );
 }
